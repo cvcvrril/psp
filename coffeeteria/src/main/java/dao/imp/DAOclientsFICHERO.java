f7c538c0aeeae9448bda7a088c568da40274d706
ines (casa) <imartinezrodriguez@educa.madrid.org>
2023-09-29 16:37:04 +0200

    29/09/2023 16:36

--- coffeeteria/src/main/java/dao/imp/DAOclientsFICHERO.java
+++ coffeeteria/src/main/java/dao/imp/DAOclientsFICHERO.java
@@ -15,7 +15,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 @Log4j2
 public class DAOclientsFICHERO implements DAOclients {
