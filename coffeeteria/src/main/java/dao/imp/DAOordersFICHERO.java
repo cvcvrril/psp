d39881bb383bb7a9ff9adba4a4a396add6958844
ines (casa) <imartinezrodriguez@educa.madrid.org>
2023-09-30 09:49:13 +0200

    30/09/2023 09:49

--- coffeeteria/src/main/java/dao/imp/DAOordersFICHERO.java
+++ coffeeteria/src/main/java/dao/imp/DAOordersFICHERO.java
@@ -1,4 +1,36 @@
 package dao.imp;
 
-public class DAOordersFICHERO {
+import dao.DAOorder;
+import io.vavr.control.Either;
+import model.Order;
+import model.errors.ErrorC;
+
+import java.util.List;
+
+public class DAOordersFICHERO implements DAOorder {
+    @Override
+    public Either<ErrorC, List<Order>> getOrder(int id) {
+        return null;
+    }
+
+    @Override
+    public Either<ErrorC, Integer> save(Order t) {
+        return null;
+    }
+
+    @Override
+    public Either<ErrorC, Integer> update(Order t) {
+        return null;
+    }
+
+    @Override
+    public Either<ErrorC, Integer> delete(Order t) {
+        return null;
+    }
+
+    @Override
+    public Either<ErrorC, List<Order>> getAll() {
+        return null;
+    }
+
 }
