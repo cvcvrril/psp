a95dd5fd310b1a3ed7a23977a55fa92b72bd0107
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-04 10:48:09 +0200

    04/10/2023 10:48

--- coffeeteria/src/main/java/model/OrderXML.java
+++ coffeeteria/src/main/java/model/OrderXML.java
@@ -2,7 +2,26 @@ package model;
 
 
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
 
-@XmlRootElement
+import java.util.List;
+
+//@XmlRootElement
+@Data
 public class OrderXML {
+
+    /*Atributos*/
+
+    private int id_ord;
+    private List<String> order_item;
+
+    /*Constructor*/
+
+    public OrderXML(int id_ord, List<String> order_item) {
+        this.id_ord = id_ord;
+        this.order_item = order_item;
+    }
+
+    /*Métodos*/
+
 }
