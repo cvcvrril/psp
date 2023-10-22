c22a5bd99849c1b19d39e6b3fb74bfe7f16302cd
ines (casa) <imartinezrodriguez@educa.madrid.org>
2023-10-22 20:05:05 +0200

    22/10/2023 20:05

--- coffeeteriadb/src/main/java/common/SQLqueries.java
+++ coffeeteriadb/src/main/java/common/SQLqueries.java
@@ -2,7 +2,7 @@ package common;
 
 public class SQLqueries {
 
-    public static final String SELECT_FROM_CUSTOMERS = "select * from customers ";
+    public static final String SELECT_FROM_CUSTOMERS = "select * from customers";
     public static final String DELETE_ORDERS = "DELETE FROM orders WHERE customer_id = ?";
     public static final String DELETE_CREDENTIALS = "DELETE FROM credentials WHERE customer_id = ?";
     public static final String DELETE_CUSTOMERS = "DELETE FROM customers WHERE id = ?";
--- coffeeteriadb/src/main/java/dao/db/DAOorderDB.java
+++ coffeeteriadb/src/main/java/dao/db/DAOorderDB.java
@@ -1,8 +1,22 @@
 package dao.db;
 
 import common.Configuration;
+import io.vavr.control.Either;
 import jakarta.inject.Inject;
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import model.Order;
+import model.errors.ErrorCOrder;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 //TODO: hacer todos los métodos
 
@@ -14,18 +28,42 @@ public class DAOorderDB {
     private final Configuration config;
     private final DBConnection db;
 
-
-
-    /*Cons*/
     @Inject
     public DAOorderDB(Configuration config, DBConnection db) {
         this.config = config;
         this.db = db;
     }
 
-    /*Methods*/
+    public Either<ErrorCOrder, List<Order>> getAll(){
+        List<Order> orderList = new ArrayList<>();
+        Either<ErrorCOrder, List<Order>> res;
+        try (Connection myConnection = db.getConnection()){
+            Statement stmt = myConnection.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from orders");
+            orderList = readRS(rs);
+            res = Either.right(orderList);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            res = Either.left(new ErrorCOrder(e.getMessage(), 0));
+        }
+        return null;
+    }
 
 
-    /*getAll()*/
+    private List<Order> readRS (ResultSet rs) throws SQLException {
+        List<Order> orderList = new ArrayList<>();
+        while (rs.next()){
+            int id = rs.getInt("order_id");
+            LocalDateTime dateTime = null;
+            Date date = rs.getDate("order_date");
+            if (date != null){
+                dateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            }
+            int customerId = rs.getInt("customer_id");
+            int tableId = rs.getInt("table_id");
+            orderList.add(new Order(id, dateTime, customerId, tableId));
+        }
+        return orderList;
+    }
 
 }
