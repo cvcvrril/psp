e084797c37fd17ca5d7cfcffddb853d23472e8fa
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-27 13:33:03 +0200

    27/10/2023 13:32

---
+++ AdivinaNumeroServ/src/main/java/servelts/PruebaServlet.java
@@ -0,0 +1,25 @@
+package servelts;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "prueba", value = "/prueba")
+public class PruebaServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int cuntador;
+        if (req.getSession().getAttribute("cuntador") == null){
+            req.getSession().setAttribute("cuntador", 0);
+        }
+
+        cuntador = (int) req.getSession().getAttribute("cuntador");
+        res.getWriter().println(cuntador);
+        cuntador++;
+        req.getSession().setAttribute("cuntador", cuntador);
+    }
+
+
+}
