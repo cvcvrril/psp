7f9895a1a66697d41a3deb54e22ed98a6b68f8fd
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-11-03 13:28:07 +0100

    03/11/2023 13:28

---
+++ coffeeteriadb/src/main/java/dao/mappers/CustomerMapper.java
@@ -0,0 +1,14 @@
+package dao.mappers;
+
+import model.Customer;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomerMapper implements RowMapper<Customer> {
+    @Override
+    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return null;
+    }
+}
