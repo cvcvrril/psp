cce80e936ca7aaca8f3b085af4ada45cb831c9ee
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2023-11-12 17:52:00 +0100

    12/11/2023 17:51

--- coffeeteriadb/src/main/java/dao/spring/DAOorderItemSpring.java
+++ coffeeteriadb/src/main/java/dao/spring/DAOorderItemSpring.java
@@ -1,8 +1,16 @@
 package dao.spring;
 
 import dao.connection.DBConnectionPool;
+import dao.mappers.OrderItemMapper;
+import io.vavr.control.Either;
 import jakarta.inject.Inject;
 import lombok.extern.log4j.Log4j2;
+import model.OrderItem;
+import model.errors.ErrorCOrder;
+import model.errors.ErrorCOrderItem;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @Log4j2
 public class DAOorderItemSpring {
@@ -14,6 +22,16 @@ public class DAOorderItemSpring {
         this.pool = pool;
     }
 
-
+    public Either<ErrorCOrderItem, List<OrderItem>> getAll(){
+        Either<ErrorCOrderItem, List<OrderItem>> res;
+        JdbcTemplate jtm = new JdbcTemplate(pool.getDataSource());
+        List<OrderItem> orderItemList = jtm.query("select * from order_items where order_item_id=?", new OrderItemMapper());
+        if (orderItemList.isEmpty()){
+            res = Either.left(new ErrorCOrderItem("error", 0));
+        }else {
+            res = Either.right(orderItemList);
+        }
+        return res;
+    }
 
 }
--- coffeeteriadb/src/main/java/services/SERVorderItem.java
+++ coffeeteriadb/src/main/java/services/SERVorderItem.java
@@ -1,6 +1,7 @@
 package services;
 
 import dao.db.DAOorderItemDB;
+import dao.spring.DAOorderItemSpring;
 import io.vavr.control.Either;
 import jakarta.inject.Inject;
 import model.OrderItem;
@@ -10,10 +11,12 @@ import java.util.List;
 
 public class SERVorderItem {
     private final DAOorderItemDB daOorderItemDB;
+    private final DAOorderItemSpring daOorderItemSpring;
 
     @Inject
-    public SERVorderItem(DAOorderItemDB daOorderItemDB) {
+    public SERVorderItem(DAOorderItemDB daOorderItemDB, DAOorderItemSpring daOorderItemSpring) {
         this.daOorderItemDB = daOorderItemDB;
+        this.daOorderItemSpring = daOorderItemSpring;
     }
 
     public Either<ErrorCOrderItem, List<OrderItem>> getOrders(int i) {
@@ -21,7 +24,8 @@ public class SERVorderItem {
     }
 
     public Either<ErrorCOrderItem, List<OrderItem>> getAll(){
-        return daOorderItemDB.getAll();
+        //return daOorderItemDB.getAll();
+        return daOorderItemSpring.getAll();
     }
 
 
