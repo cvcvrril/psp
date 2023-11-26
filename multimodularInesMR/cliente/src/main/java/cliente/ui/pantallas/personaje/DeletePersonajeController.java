5fbf0f30ecd229dea3517c51081938b9e4d071bc
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2023-11-26 13:13:43 +0100

    26/11/2023 13:13

--- multimodularInesMR/cliente/src/main/java/cliente/ui/pantallas/personaje/DeletePersonajeController.java
+++ multimodularInesMR/cliente/src/main/java/cliente/ui/pantallas/personaje/DeletePersonajeController.java
@@ -1,4 +1,66 @@
 package cliente.ui.pantallas.personaje;
 
-public class DeletePersonajeController {
+import cliente.domain.usecases.DeletePersonajeUseCase;
+import cliente.domain.usecases.GetAllPersonajesUseCase;
+import cliente.ui.pantallas.common.BasePantallaController;
+import domain.modelo.Personaje;
+import jakarta.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
+
+public class DeletePersonajeController extends BasePantallaController {
+    
+    private DeletePersonajeViewModel viewModel;
+    private final DeletePersonajeUseCase deleteUseCase;
+    private final GetAllPersonajesUseCase allUseCase;
+    
+    @FXML
+    private TableView<Personaje> tablaPersonajes;
+    @FXML
+    private TableColumn<Personaje, Integer> idPersonajeCol;
+    @FXML
+    private TableColumn<Personaje, String> nombrePersonajeCol;
+    @FXML
+    private TableColumn<Personaje, Integer> razaPersonajeCol;
+    @FXML
+    private TableColumn<Personaje, String> planetaResPersonajeCol;
+
+    @Inject
+    public DeletePersonajeController(DeletePersonajeViewModel viewModel, DeletePersonajeUseCase deleteUseCase, GetAllPersonajesUseCase allUseCase) {
+        this.viewModel = viewModel;
+        this.deleteUseCase = deleteUseCase;
+        this.allUseCase = allUseCase;
+    }
+    
+    @Override
+    public void principalCargado(){
+        viewModel = new DeletePersonajeViewModel(allUseCase, deleteUseCase);
+        viewModel.loadAllPersonajes();
+        viewModel.getState().addListener((observable, oldValue, newValue) ->
+                rellena(newValue));
+        
+    }
+
+    private void rellena(DeletePersonajeState state) {
+        idPersonajeCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nombrePersonajeCol.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        razaPersonajeCol.setCellValueFactory(new PropertyValueFactory<>("raza"));
+        planetaResPersonajeCol.setCellValueFactory(new PropertyValueFactory<>("planetaRes"));
+        List<Personaje> personajes = state.getPersonajes();
+        if (personajes != null) {
+            tablaPersonajes.getItems().setAll(personajes);
+        }
+    }
+
+    @FXML
+    private void delete(){
+        Personaje personajeSeleccionado = tablaPersonajes.getSelectionModel().getSelectedItem();
+        if (personajeSeleccionado!=null){
+            viewModel.deletePersonaje(personajeSeleccionado.getId());
+        }
+    }
 }
--- multimodularInesMR/cliente/src/main/java/cliente/ui/pantallas/personaje/DeletePersonajeViewModel.java
+++ multimodularInesMR/cliente/src/main/java/cliente/ui/pantallas/personaje/DeletePersonajeViewModel.java
@@ -1,4 +1,60 @@
 package cliente.ui.pantallas.personaje;
 
+import cliente.domain.usecases.DeletePersonajeUseCase;
+import cliente.domain.usecases.GetAllPersonajesUseCase;
+import domain.errores.ApiError;
+import jakarta.inject.Inject;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import lombok.extern.log4j.Log4j2;
+
+import java.time.LocalDateTime;
+
+@Log4j2
 public class DeletePersonajeViewModel {
+
+    private final GetAllPersonajesUseCase getAllPersonajesUseCase;
+    private final DeletePersonajeUseCase deletePersonajeUseCase;
+    private ObjectProperty<DeletePersonajeState> _state;
+
+    @Inject
+    public DeletePersonajeViewModel(GetAllPersonajesUseCase getAllPersonajesUseCase, DeletePersonajeUseCase deletePersonajeUseCase) {
+        this.getAllPersonajesUseCase = getAllPersonajesUseCase;
+        this.deletePersonajeUseCase = deletePersonajeUseCase;
+        _state = new SimpleObjectProperty<>(new DeletePersonajeState(null, null, null));
+    }
+
+    public void loadAllPersonajes() {
+        try {
+            getAllPersonajesUseCase.getAllPersonajes()
+                    .subscribe(res -> {
+                                if (res.isLeft()) {
+                                    _state.setValue(new DeletePersonajeState(null, new ApiError("Error al obtener los personajes", LocalDateTime.now()), null));
+                                } else {
+                                    _state.setValue(new DeletePersonajeState(null, null, res.get()));
+                                }
+                            },
+                            error -> log.error("Error al obtener los personajes", error));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public void deletePersonaje(int id) {
+        deletePersonajeUseCase.deletePersonaje(id)
+                .subscribe(res -> {
+                            if (res.isLeft()) {
+                                _state.setValue(new DeletePersonajeState(null, new ApiError("Error al eliminar el personaje", LocalDateTime.now()), null));
+                            } else {
+                                loadAllPersonajes();
+                            }
+                        }, error -> log.error("Error al eliminar el personaje", error));
+
+    }
+
+    public ReadOnlyObjectProperty<DeletePersonajeState> getState() {
+        return _state;
+    }
+
 }
