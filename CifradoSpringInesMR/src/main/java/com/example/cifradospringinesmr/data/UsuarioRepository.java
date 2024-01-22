729ffaed9fdfbc12bd3f55f3bac735d077ce90ef
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-01-22 20:46:42 +0100

    22/01/2024 20:46

--- CifradoSpringInesMR/src/main/java/com/example/cifradospringinesmr/data/UsuarioRepository.java
+++ CifradoSpringInesMR/src/main/java/com/example/cifradospringinesmr/data/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package com.example.cifradospringinesmr.data;
 
-public class UsuarioRepository {
+import com.example.cifradospringinesmr.data.model.User;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends ListCrudRepository<User, UUID>{
+
+
 }
--- CifradoSpringInesMR/src/main/java/com/example/cifradospringinesmr/data/model/User.java
+++ CifradoSpringInesMR/src/main/java/com/example/cifradospringinesmr/data/model/User.java
@@ -1,14 +1,25 @@
 package com.example.cifradospringinesmr.data.model;
 
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Column(name = "username", nullable = false)
     private String username;
+    @Column(name = "password", nullable = false)
     private String password;
 }
