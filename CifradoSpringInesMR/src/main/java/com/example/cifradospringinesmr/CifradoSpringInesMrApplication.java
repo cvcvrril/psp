e383ac4672bf9f156a3bf43190db62d5463efed7
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-01-22 20:18:52 +0100

    22/01/2024 20:18

---
+++ CifradoSpringInesMR/src/main/java/com/example/cifradospringinesmr/CifradoSpringInesMrApplication.java
@@ -0,0 +1,13 @@
+package com.example.cifradospringinesmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifradoSpringInesMrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CifradoSpringInesMrApplication.class, args);
+    }
+
+}
---
+++ CifradoSpringInesMR/src/test/java/com/example/cifradospringinesmr/CifradoSpringInesMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cifradospringinesmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifradoSpringInesMrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
