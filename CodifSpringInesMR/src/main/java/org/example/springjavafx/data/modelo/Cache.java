347742ab45a42a7747f9d26341ace913752d4c60
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-01-24 20:07:03 +0100

    24/01/2024 20:07

--- CodifSpringInesMR/src/main/java/org/example/springjavafx/data/modelo/Cache.java
+++ CodifSpringInesMR/src/main/java/org/example/springjavafx/data/modelo/Cache.java
@@ -1,5 +1,12 @@
 package org.example.springjavafx.data.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
 public class Cache {
 
     private String userPassword;
