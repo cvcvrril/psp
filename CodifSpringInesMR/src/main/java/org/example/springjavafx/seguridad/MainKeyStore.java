32454e4ecd24817e5fb27bbe7ba4b34df46b8a34
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-01-26 08:45:47 +0100

    26/01/2024 08:45

--- CodifSpringInesMR/src/main/java/org/example/springjavafx/seguridad/MainKeyStore.java
+++ CodifSpringInesMR/src/main/java/org/example/springjavafx/seguridad/MainKeyStore.java
@@ -1,14 +1,20 @@
 package org.example.springjavafx.seguridad;
 
+import org.bouncycastle.jce.X509Principal;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 import static org.bouncycastle.oer.its.etsi102941.CtlDelete.cert;
 
@@ -25,6 +31,16 @@ public class MainKeyStore {
             PublicKey clavePublica = clavesRSA.getPublic();
 
             X509V3CertificateGenerator cert1 = new X509V3CertificateGenerator();
+
+            cert1.setSerialNumber(BigInteger.valueOf(1));   //or generate a random number
+            cert1.setSubjectDN(new X509Principal("CN=Server"));  //see examples to add O,OU etc
+            cert1.setIssuerDN(new X509Principal("CN=Server")); //same since it is self-signed
+            cert1.setPublicKey(clavesRSA.getPublic());
+            cert1.setNotBefore(
+                    Date.from(LocalDate.now().plus(365, ChronoUnit.DAYS).atStartOfDay().toInstant(ZoneOffset.UTC)));
+            cert1.setNotAfter(new Date());
+            cert1.setSignatureAlgorithm("SHA256WithRSAEncryption");
+
             X509Certificate cert =  cert1.generate(clavePrivada);
             KeyStore ks = KeyStore.getInstance("PKCS12");
             char[] password = "Obito".toCharArray();        // -> Tomar la contraseña de aquí desde el Config
