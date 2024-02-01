7814abe71ffdcf1b355b5441b300ae2b72968702
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-02-01 21:53:45 +0100

    01/02/2024 21:53

---
+++ GraphQLServerInesMR/src/main/kotlin/com/example/graphqlserverinesmr/GraphQlServerInesMrApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlserverinesmr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphQlServerInesMrApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphQlServerInesMrApplication>(*args)
+}
---
+++ GraphQLServerInesMR/src/main/kotlin/com/example/graphqlserverinesmr/config/Configuration.kt
@@ -0,0 +1,10 @@
+package com.example.graphqlserverinesmr.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Configuration {
+
+
+
+}
