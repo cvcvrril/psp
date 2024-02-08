4ed961524b081e7ea596ca66301387f48d62c739
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-02-08 12:57:40 +0100

    08/02/2024 12:57

--- GraphQLServerJavaInesMR/src/main/resources/data.sql
+++ GraphQLServerJavaInesMR/src/main/resources/data.sql
@@ -4,6 +4,13 @@ delete from videojuegos;
 delete from roles;
 delete from users;
 
+insert into roles (id, rol) values (1, 'ROLE_ADMIN');
+insert into roles (id, rol) values (1, 'ROLE_USER');
+
+insert into 
+
+insert into users()
+
 insert into videojuegos(id, titulo, descripcion) values (1, 'Grand Thief Auto V', 'Prueba prueba');
 
 insert into personajes (id, nombre, descripcion, id_videojuego) values (1, 'Franklin', 'Eoooo', 1);
