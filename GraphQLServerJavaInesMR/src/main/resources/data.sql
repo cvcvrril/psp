c67416e6da7b526cf8c4df5d8dfbee8dcfcb4a1d
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-02-08 13:28:04 +0100

    08/02/2024 13:28

--- GraphQLServerJavaInesMR/src/main/resources/data.sql
+++ GraphQLServerJavaInesMR/src/main/resources/data.sql
@@ -5,7 +5,7 @@ delete from roles;
 delete from users;
 
 insert into roles (id, rol) values (1, 'ROLE_ADMIN');
-insert into roles (id, rol) values (1, 'ROLE_USER');
+insert into roles (id, rol) values (2, 'ROLE_USER');
 
 insert into users (id, username, password) values (1, 'ines', '$2a$10$QmslVN.VYaukSrN03XSVSukpEiiG//MpvBh2KzG3mxk5kXlC4A5Em');
 
