c784de6d241630088c056642b9e326dbf4e930e9
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-02-14 13:08:05 +0100

    14/02/2024 13:08

--- AprobarInesMR/GraphQL/src/main/java/com/example/graphql/ui/controllers/UserController.java
+++ AprobarInesMR/GraphQL/src/main/java/com/example/graphql/ui/controllers/UserController.java
@@ -3,11 +3,22 @@ package com.example.graphql.ui.controllers;
 
 import com.example.graphql.domain.modelo.User;
 import com.example.graphql.domain.servicio.UserServicio;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +30,30 @@ public class UserController {
     @PostMapping("/registro")
     public User registro(@RequestBody User newUser){
         return servicio.add(newUser);
+    }
+
+
+    private String generarTokenJWT(int expirationSeconds, String username, String rol) {
+        final MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            //throw new WrongObjectException(ConstantsJakarta.HUBO_UN_PROBLEMA);
+            throw new RuntimeException("Hubo un problema");
+        }
+        digest.update("clave".getBytes(StandardCharsets.UTF_8));
+        final SecretKeySpec key2 = new SecretKeySpec(
+                digest.digest(), 0, 64, "AES");
+        SecretKey keyConfig = Keys.hmacShaKeyFor(key2.getEncoded());
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuer("self")
+                .setExpiration(Date
+                        .from(LocalDateTime.now().plusSeconds(expirationSeconds).atZone(ZoneId.systemDefault())
+                                .toInstant()))
+                .claim("rol", rol)
+                .signWith(keyConfig).compact();
     }
 
 
